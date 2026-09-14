5dc37f29474b8d5aec1c491477f23342f9de1d09
HaydenQ-creator <hquach@ftstudent.org>
2026-09-14 19:53:39 -0400

    Unfinished Floating island

--- survival-the-lava-2/FloatingLand.cs
+++ survival-the-lava-2/FloatingLand.cs
@@ -1,14 +1,17 @@
 using Godot;
 using System;
 
-public partial class FloatingLand : Node3D
+public partial class FloatingLand : CharacterBody3D
 {
 	public static FloatingLand Instance { get; private set; }
 	
 	public const float Speed = 5.0f;
-	public static bool is_touched = false;
+	public static bool is_touched = true;
 
-
+	public override void _Process(double delta)
+	{
+		GD.Print(is_touched);
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
