5cde8d7c0200cc9ced1ed58f0f2869a92911c335
HaydenQ-creator <hquach@ftstudent.org>
2026-09-18 01:58:08 -0400

    added main menu

---
+++ survival-the-lava-2/MainMenu.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	private Button _startButton;
+	private Button _quitButton;
+
+	public override void _Ready()
+	{
+		_startButton = GetNode<Button>("StartButton");
+		_startButton.Pressed += OnStartButtonPressed;
+		
+		_quitButton = GetNode<Button>("QuitButton");
+		_quitButton.Pressed += OnQuitButtonPressed;
+	}
+
+	private void OnStartButtonPressed()
+	{
+		string gameScenePath = "res://Test.tscn"; 
+
+		GetTree().ChangeSceneToFile(gameScenePath);
+	}
+	private void OnQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
+}
